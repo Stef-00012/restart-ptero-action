db2b775b887d5b498bd8554472aad50cb7af34ef
Stef-00012 <me@stefdp.com>
2026-07-23 17:35:19 +0200

    add pre-restart command and restart delay

---
+++ README.md
@@ -0,0 +1,26 @@
+# restart Ptero Action
+
+A GitHub action to restart your Pterodactyl Panel servers
+
+# Usage
+
+Inside `steps`:
+
+```yml
+- uses: https://git.stefdp.com/Stef/restart-ptero-action@main
+  with:
+    PTERO_PANEL_URL: ${{ secrets.PTERO_PANEL_URL }}
+    PTERO_PANEL_TOKEN: ${{ secrets.PTERO_PANEL_TOKEN }}
+    PTERO_PANEL_SERVER_ID: ${{ secrets.PTERO_PANEL_SERVER_ID }}
+```
+
+## Variables
+
+| Variable | Required | Description |
+|    :-:   |    :-:   |     :-:     |
+| `PTERO_PANEL_URL` | Yes | Server URL of your Pterodactyl Panel |
+| `PTERO_PANEL_TOKEN` | Yes | API Key (created via `/account/api`) |
+| `PTERO_PANEL_SERVER_ID` | Yes | ID of the server you want to restart |
+| `PRE_RESTART_COMMAND` | No | A command to run before the server restarts |
+| `RESTART_DELAY` | No | Amount of seconds to wait before restarting the server (useful with `PRE_RESTART_COMMAND`) |
+| `PRE_RESTART_COMMAND_STOP_ON_ERROR` | No | Whether to restart the server if the command HTTP request fails (**NOTE**: This is only for the HTTP request, it won't stop if the command fails as the API doesn't say if the command succeeds or not) |
--- src/index.js
+++ src/index.js
@@ -3,43 +3,103 @@ const core = require("@actions/core");
 const panelUrl = core.getInput("PTERO_PANEL_URL");
 const apiToken = core.getInput("PTERO_PANEL_TOKEN");
 const serverId = core.getInput("PTERO_PANEL_SERVER_ID");
+const preRestartCommand = core.getInput("PRE_RESTART_COMMAND");
+const restartDelay = core.getInput("RESTART_DELAY");
+const restartDelayInt = parseInt(restartDelay, 10);
+const stopOnError = core.getInput("PRE_RESTART_COMMAND_STOP_ON_ERROR") === "true";
 
 if (!panelUrl) {
 	core.error("Missing Pterodactyl Panel URL");
+	
 	process.exit(1);
 }
 if (!apiToken) {
 	core.error("Missing Pterodactyl API Token");
+	
 	process.exit(1);
 }
 if (!serverId) {
 	core.error("Missing Pterodactyl Server ID");
+	
 	process.exit(1);
 }
 
-fetch(`${panelUrl}/api/client/servers/${serverId}/power`, {
-	method: "POST",
-	headers: {
-		"Content-type": "application/json",
-		Authorization: `Bearer ${apiToken}`,
-	},
-	body: JSON.stringify({
-		signal: "restart",
-	}),
-})
-	.then(async (res) => {
+if (restartDelay && (Number.isNaN(restartDelayInt) || restartDelayInt <= 0)) {
+	core.error("Restart delay must be a non-negative integer");
+	
+	process.exit(1);
+}
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function main() {
+	if (preRestartCommand) {
+		try {
+			core.info("Running pre-restart command...");
+
+			const res = await fetch(`${panelUrl}/api/client/servers/${serverId}/command`, {
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+					Authorization: `Bearer ${apiToken}`,
+				},
+				body: JSON.stringify({
+					command: preRestartCommand,
+				}),
+			})
+
+			if (!res.ok) {
+				const errorMessage = await res.text();
+
+				core.error(`Failed to execute pre-restart command. HTTP code ${res.status}. Message: ${errorMessage}`);
+
+				if (stopOnError) {
+					process.exit(1);
+				}
+			}
+		} catch(e) {
+			core.error(`Error while executing pre-restart command: ${e.message}`);
+
+			if (stopOnError) {
+				process.exit(1);
+			}
+		}
+	}
+
+	if (restartDelay) {
+			core.info(`Waiting for ${restartDelayInt} seconds before restarting the server...`);
+
+			await sleep(restartDelayInt * 1000);
+		}
+
+	try {
+		core.info("Restarting the server...")
+
+		const res = await fetch(`${panelUrl}/api/client/servers/${serverId}/power`, {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json",
+				Authorization: `Bearer ${apiToken}`,
+			},
+			body: JSON.stringify({
+				signal: "restart",
+			}),
+		})
+
 		if (res.ok) return process.exit();
 
+		const errorMessage = await res.text();
+
+		core.error(`Something went wrong while sending the request, HTTP code ${res.status}. Message: ${errorMessage}`);
+
+		process.exit(1);
+	} catch(e) {
 		core.error(
-			`Something went wrong while sending the request, HTTP code ${res.status}. Message: ${await res.text()}`,
+			`Something went wrong while sending the request. Message: ${e.message}`,
 		);
 
 		process.exit(1);
-	})
-	.catch(async (error) => {
-		core.error(
-			`Something went wrong while sending the request. Message: ${error}`,
-		);
+	}
+}
 
-		process.exit(1);
-	});
+main();
